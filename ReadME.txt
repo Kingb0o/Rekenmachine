5584e23e74b65f9594b0b651589cd7f92cf6a608
Wessel van der Linde <47216932+Kingb0o@users.noreply.github.com>
2020-11-09 17:16:05 +0100

    Add files via upload

---
+++ ReadME.txt
@@ -0,0 +1,9 @@
+Dit programma functioneert als een standaard rekenmachine. Er kan dan ook een operatie worden uitgevoerd op 2
+verschillende waardes. De rekenmachine beschikt verder ook over trigonomische functionaliteiten. Zo kunnen er
+berekeningen worden gedaan met sin, cos en tan. Tot slot beschikt deze ook over een geheugen. Hier kan tijdelijk een
+waarde in worden opgeslagen om later in de berekening weer te gebruiken.
+
+Bij het maken van deze website is geen gebruik gemaakt van externe tools en/of libriraries. De website kan dus direct in 
+de browser worden geopend en de rekenmachine is dan meteen functioneel.
+
+Geschreven door Wessel van der Linde, 9 Nov 2020.
---
+++ script.js
@@ -0,0 +1,131 @@
+/* Calculator object */
+const calculator = {
+    display: '0',
+    operand: null,
+    firstVal: '0',
+    lastOp: true,
+    err: false
+};
+
+/* Memory object. */
+const memory = {
+    value: '0'
+};
+
+/* Maximum amount of numbers that the display can display. */
+const displayLength = 14;
+
+/* Handles memory functionality */
+function updateMemory(mem) {
+    if (mem == 'MR') {
+        calculator.display = memory.value;
+    } else if (mem == 'MS') {
+        memory.value = calculator.display;
+    } else {
+        memory.value = '0';
+    }
+    updateDisplay();
+}
+
+/* Handles operators and stores display value if necessary. */
+function updateCalc(operator) {
+    calculator.operand = operator;
+
+    if (calculator.lastOp == false) {
+        calculator.firstVal = calculator.display;
+    }
+    calculator.display = operator;
+    calculator.lastOp = true;
+    updateDisplay();
+}
+
+/* Updates value from input and stores it accordingly. */
+function updateValue(newVal) {
+    if (calculator.display === '0' || calculator.lastOp || calculator.err) {
+        calculator.display = newVal;
+        calculator.err = false;
+        calculator.lastOp = false;
+    } else {
+        if (calculator.display.toString().length > displayLength) {
+            return;
+        }
+        calculator.display = calculator.display + newVal;
+    }
+    updateDisplay();
+}
+
+/* Clears all stored variables in the calculator object. */
+function updateClear() {
+    calculator.display = '0';
+    calculator.firstVal = '0';
+    calculator.operand = null;
+    calculator.lastOp = true;
+    updateDisplay();
+}
+
+/* Updates the display on the calculator. */
+function updateDisplay() {
+    document.getElementById("screen").innerHTML = calculator.display;
+}
+
+/* Calculates the output for the first value, the operator and the second value. */
+function calculate() {
+    if (!calculator.operand) {
+        return;
+    }
+
+    let res = {
+        '+': function (x, y) { return x + y },
+        '-': function (x, y) { return x - y },
+        '*': function (x, y) { return x * y },
+        '/': function (x, y) { return x / y },
+        'sin(x)': function (x, y) { return Math.sin(y * Math.PI / 180) },
+        'cos(x)': function (x, y) { return Math.cos(y * Math.PI / 180) },
+        'tan(x)': function (x, y) { return Math.tan(y * Math.PI / 180) }
+    }
+    calculator.display = res[calculator.operand](parseFloat(calculator.firstVal), parseFloat(calculator.display));
+    calculator.display = +(Math.round(calculator.display + "e+2")  + "e-2");
+    calculator.firstVal = calculator.display;
+
+    if (calculator.display.toString().length > displayLength) {
+        calculator.display = "NaN";
+        calculator.err = true;
+
+    }
+    updateDisplay();
+}
+
+/* Evenlistener for buttons on the calculator */
+const keys = document.querySelector('.keys');
+
+keys.addEventListener('click', (event) => {
+    const { target } = event;
+
+    if (!target.matches('button')) {
+        return;
+    }
+
+    if (target.classList.contains('mem')) {
+        updateMemory(target.value);
+    }
+
+    if (target.classList.contains('operator')) {
+        updateCalc(target.value);
+        return;
+    }
+
+    if (target.classList.contains('num')) {
+        updateValue(target.value);
+        return;
+    }
+
+    if (target.classList.contains('clear')) {
+        updateClear();
+        return;
+    }
+
+    if (target.classList.contains('equal')) {
+        calculate();
+        return;
+    }
+});
